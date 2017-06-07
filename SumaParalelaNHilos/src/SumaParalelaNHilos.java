395587d8359b6eed4be7fde7cd400abf2afe75f3
Chalo Salvador <chalosalvador@gmail.com>
2017-06-07 14:51:54 -0500

    examen

---
+++ SumaParalelaNHilos/src/SumaParalelaNHilos.java
@@ -0,0 +1,18 @@
+/*
+Escribir un programa que lea una lista de números desde un archivo.
+El objetivo del programa es presentar por pantalla la suma total de la lista de números.
+El programa debe predir al usuario que ingrese la cantidad de hilos que desea crear.
+Se debe dividir la lista de números en la cantidad hilos que especifique el usuario y cada parte de la lista
+asignarle a un hilo.
+La lista de números solo puede contener una cantidad par de elementos y la cantidad de hilos que especifique el
+usuario debe ser también un número par.
+Cada hilo debe realizar la suma de la sublista de números que ha recibido
+El hilo principal debe encargarse de recibir los resultados parciales de cada hilo y sumarlas
+para presentar el resultado total.
+ */
+
+/**
+ * Created by chalosalvador on 7/6/17.
+ */
+public class SumaParalelaNHilos {
+}
